5947a7ff1acf8f0dc83574282ce0bf124599daaa
Daniliva <53309093+Daniliva@users.noreply.github.com>
2020-07-23 17:35:08 +0300

    First use for use DB

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer;
+using DataLayer.Entityes;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using WebApplication1.Models;
 
@@ -12,17 +15,19 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private EFDBContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, EFDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             HelloModel _model = new HelloModel() { HelloMessage = "I see you!" };
-
-            return View(_model);
+            List<Directory> _dirs = _context.Directory.Include(x=>x.Materials).ToList();
+            return View(_dirs);
         }
 
         public IActionResult Privacy()
